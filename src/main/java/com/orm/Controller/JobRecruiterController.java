03dbe25b02c09353d3c11aacc639573bd5f40997
Abishek SB <75730358+AbishekSB@users.noreply.github.com>
2024-05-11 15:10:17 +0530

    Update JobRecruiterController.java

--- src/main/java/com/orm/Controller/JobRecruiterController.java
+++ src/main/java/com/orm/Controller/JobRecruiterController.java
@@ -4,13 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.orm.entity.JobRecruiter;
@@ -33,27 +29,16 @@ public class JobRecruiterController {
 			repo.createJobRecruiter(jobRecruiter);
 			msg = " Object Saved";
 		} catch (Exception e) {
-//			msg = " Object Not Saved";
 			e.printStackTrace();
 		}
 		return msg;
 	}
-	
-//	@GetMapping("/GetJobRecruiterById/{providerId}")
-//	public JobRecruiter getJobRecruiterById(@PathVariable("providerId")int providerId) {
-//		JobRecruiter recruiter = repo.findById(providerId).get();	
-//		
-//		return recruiter;
-//	}
-	
-	
-	
+		
+		
 	@GetMapping("getSeekerIdList")
 	List<Integer> getIdListtt(){
 		return repo.getIdListtt();
-		
-
-		
+				
 	}
 	
 	@GetMapping("getAllDetailsOwner")
